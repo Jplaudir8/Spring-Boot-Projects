ef9f4d33eed3f2a91869055f9e0242c4cdd1f804
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-09-11 16:24:21 -0500

    added thymeleaf att, refactored services package name, started PostmMapping method, created UserMapper

--- Cloud-Storage-Service-Company/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ Cloud-Storage-Service-Company/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -1,15 +1,30 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/signup")
 public class SignupController {
 
+    private final
+
     @GetMapping
     public String getSignupPage() {
+        return "signup";
+    }
+
+    @PostMapping
+    public String signupUser(@ModelAttribute User user, Model model){
+        String signupError = null; // this variable is read by signup.html
+
+        // Implementing signup security...
+
         return "signup";
     }
 }
---
+++ Cloud-Storage-Service-Company/src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/UserService.java
@@ -0,0 +1,7 @@
+package com.udacity.jwdnd.course1.cloudstorage.service;
+
+public class UserService {
+
+    // implementing UserMapper mapper
+
+}
