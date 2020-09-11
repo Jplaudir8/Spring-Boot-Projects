a6ea7e89d1359aff338660a9be348c4a9f429ae7
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-09-11 16:28:53 -0500

    dependency injection in SignupController(with UserService class)

--- Cloud-Storage-Service-Company/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ Cloud-Storage-Service-Company/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -1,6 +1,9 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.service.HashService;
+import com.udacity.jwdnd.course1.cloudstorage.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/signup")
 public class SignupController {
 
-    private final
+    private final UserService userService;
+
+    public SignupController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public String getSignupPage() {
