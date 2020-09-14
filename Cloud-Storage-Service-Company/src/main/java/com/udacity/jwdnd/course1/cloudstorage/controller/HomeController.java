4e2687d51b70e3e2b0da2f297a3f451ad1c7a426
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-09-14 07:52:31 -0500

    testing

--- Cloud-Storage-Service-Company/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ Cloud-Storage-Service-Company/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.NoteModalForm;
 import com.udacity.jwdnd.course1.cloudstorage.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/home")
 public class HomeController {
 
-    NoteService noteService;
+    @Autowired
+    private NoteService noteService;
 
     public HomeController(NoteService noteService) {
         this.noteService = noteService;
     }
 
     @GetMapping
-    public String getHomePage(NoteModalForm noteModalForm, Model model) {
+    public String getHomePage(NoteModalForm noteModalForm, Model model) throws Exception {
         model.addAttribute("notes", this.noteService.getAllNotes());
         return "home";
     }
