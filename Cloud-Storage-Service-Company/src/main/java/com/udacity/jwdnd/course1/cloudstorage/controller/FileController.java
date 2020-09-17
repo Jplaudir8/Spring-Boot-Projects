6f7f96aa833ad1590cc6f3592433867047213353
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-09-17 12:55:51 -0500

    working on createFile method

--- Cloud-Storage-Service-Company/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
+++ Cloud-Storage-Service-Company/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
@@ -1,12 +1,22 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/file")
 public class FileController {
 
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("fileUpload") MultipartFile fileUpload, Authentication authentication, Model model, RedirectAttributes redirectAttributes) {
 
+        return "redirect:/result";
+    }
 
 }
--- Cloud-Storage-Service-Company/src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/FileService.java
+++ Cloud-Storage-Service-Company/src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/FileService.java
@@ -1,15 +1,36 @@
 package com.udacity.jwdnd.course1.cloudstorage.service;
 
 import com.udacity.jwdnd.course1.cloudstorage.mapper.FileMapper;
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
 public class FileService {
 
     private FileMapper fileMapper;
+    private UserMapper userMapper;
 
-    public FileService(FileMapper fileMapper) {
+    public FileService(FileMapper fileMapper, UserMapper userMapper) {
         this.fileMapper = fileMapper;
+        this.userMapper = userMapper;
+    }
+
+    public List<File> getAllUserFiles(int userId) {
+        return fileMapper.getAllUserFiles(userId);
+    }
+
+    public void createFile(MultipartFile fileUpload, String username) {
+        int userId = userMapper.getUserIdByUsername(username);
+        File newFile = new File(null, fileUpload.getOriginalFilename(),
+                fileUpload.getContentType(),
+                String.valueOf(fileUpload.getSize()),
+                userId,
+                fileUpload.getBytes());
+        fileMapper.insertFile(newFile);
     }
 
 }
