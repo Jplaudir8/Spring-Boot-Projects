b8952cd24b08e0a7532832bd83dee4603bdb3c09
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-09-25 12:15:01 -0500

    added exceptions, mutator and resolver

---
+++ Review-Exercises/DogGraphQL/src/main/java/com/udacity/DogGraphQL/exception/BreedNotFoundException.java
@@ -0,0 +1,37 @@
+package com.udacity.DogGraphQL.exception;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import graphql.ErrorType;
+import graphql.GraphQLError;
+import graphql.language.SourceLocation;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BreedNotFoundException extends RuntimeException implements GraphQLError {
+
+    private Map<String, Object> extensions = new HashMap<>();
+
+    public BreedNotFoundException(String message, String invalidBreed) {
+        super(message);
+        extensions.put("invalidBreedId", invalidBreed);
+    }
+
+    @Override
+    public List<SourceLocation> getLocations() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getExtensions() {
+        return extensions;
+    }
+
+    @Override
+    public ErrorType getErrorType() {
+        return ErrorType.DataFetchingException;
+    }
+
+
+}
