5eb79d73b220279d54226cb98b235f1de5cf7da2
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-09-24 22:12:34 -0500

    added graphql schema, entity and repo

---
+++ Review-Exercises/DogGraphQL/src/main/java/com/udacity/DogGraphQL/entity/Dog.java
@@ -0,0 +1,34 @@
+package com.udacity.DogGraphQL.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Dog {
+    private String name;
+    private String breed;
+    private String origin;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+}
