af5761880730eece6d79e6a12baeb41d0338722f
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-09-28 10:53:22 -0500

    added annotation

--- Review-Exercises/Microservices/DogMicroservice/src/main/java/com/udacity/DogMicroservice/DogMicroserviceApplication.java
+++ Review-Exercises/Microservices/DogMicroservice/src/main/java/com/udacity/DogMicroservice/DogMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.udacity.DogMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient // This annotation is optional here if the spring-cloud-starter-netflix-eureka-client dependency is on the classpath already.
 public class DogMicroserviceApplication {
 
 	public static void main(String[] args) {
