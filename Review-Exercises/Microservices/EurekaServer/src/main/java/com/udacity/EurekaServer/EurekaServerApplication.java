018103fe5fa32e48bc9a7789700ae1394915bb55
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-09-27 22:49:22 -0500

    added some configurations and dependency

--- Review-Exercises/Microservices/EurekaServer/src/main/java/com/udacity/EurekaServer/EurekaServerApplication.java
+++ Review-Exercises/Microservices/EurekaServer/src/main/java/com/udacity/EurekaServer/EurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.udacity.EurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // This annotation tells Spring to activate the Eureka server related configuration.
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
