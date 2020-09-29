f493576c4f5ebcc627813facab3dd6dfd4f6e5f0
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-09-29 12:51:42 -0500

    added Kanye West API

---
+++ Review-Exercises/KanyeWestAPI/src/main/java/com/udacity/KanyeWestAPI/KanyeWestApiApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.KanyeWestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanyeWestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanyeWestApiApplication.class, args);
+	}
+
+}
