11f11e644f6befdbe0419e4ac9bca9283c4a725e
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-09-29 19:03:50 -0500

    added custom response messages

--- Review-Exercises/DogRestAPI(+SwaggerDoc)/src/main/java/com/udacity/DogRestAPI/config/SwaggerConfig.java
+++ Review-Exercises/DogRestAPI(+SwaggerDoc)/src/main/java/com/udacity/DogRestAPI/config/SwaggerConfig.java
@@ -23,7 +23,8 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false);
     }
 
     private ApiInfo apiInfo() {
