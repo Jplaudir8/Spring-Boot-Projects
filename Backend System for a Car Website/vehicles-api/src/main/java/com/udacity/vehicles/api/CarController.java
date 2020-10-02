55a88ae9aabd1ec4fbe0fdab3a428428fd446408
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-10-02 16:43:34 -0500

    added CarService functionality

--- Backend System for a Car Website/vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
+++ Backend System for a Car Website/vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
@@ -59,10 +59,11 @@ class CarController {
     @GetMapping("/{id}")
     Resource<Car> get(@PathVariable Long id) {
         /**
-         * TODO: Use the `findById` method from the Car Service to get car information.
-         * TODO: Use the `assembler` on that car and return the resulting output.
+         * Use the `findById` method from the Car Service to get car information.
+         * Use the `assembler` on that car and return the resulting output.
          *   Update the first line as part of the above implementing.
          */
+        carService.findById(id);
         return assembler.toResource(new Car());
     }
 
