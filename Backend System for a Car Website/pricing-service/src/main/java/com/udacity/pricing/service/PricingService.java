9edfe3f9ab85252b8aa422d82bc374ecc1ae15dd
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-10-02 15:55:28 -0500

    commented out PricingService

--- Backend System for a Car Website/pricing-service/src/main/java/com/udacity/pricing/service/PricingService.java
+++ Backend System for a Car Website/pricing-service/src/main/java/com/udacity/pricing/service/PricingService.java
@@ -13,21 +13,26 @@ import java.util.stream.LongStream;
  * Implements the pricing service to get prices for each vehicle.
  */
 public class PricingService {
+/*
 
-    /**
+    */
+/**
      * Holds {ID: Price} pairings (current implementation allows for 20 vehicles)
-     */
+     *//*
+
     private static final Map<Long, Price> PRICES = LongStream
             .range(1, 20)
             .mapToObj(i -> new Price("USD", randomPrice(), i))
             .collect(Collectors.toMap(Price::getVehicleId, p -> p));
 
-    /**
+    */
+/**
      * If a valid vehicle ID, gets the price of the vehicle from the stored array.
      * @param vehicleId ID number of the vehicle the price is requested for.
      * @return price of the requested vehicle
      * @throws PriceException vehicleID was not found
-     */
+     *//*
+
     public static Price getPrice(Long vehicleId) throws PriceException {
 
         if (!PRICES.containsKey(vehicleId)) {
@@ -37,13 +42,16 @@ public class PricingService {
         return PRICES.get(vehicleId);
     }
 
-    /**
+    */
+/**
      * Gets a random price to fill in for a given vehicle ID.
      * @return random price for a vehicle
-     */
+     *//*
+
     private static BigDecimal randomPrice() {
         return new BigDecimal(ThreadLocalRandom.current().nextDouble(1, 5))
                 .multiply(new BigDecimal(5000d)).setScale(2, RoundingMode.HALF_UP);
     }
+*/
 
 }
