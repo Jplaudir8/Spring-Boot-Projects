37e9b188ed0dbfe8b3b07b151d6e94487c923b9f
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-10-07 12:13:14 -0500

    added added controller class

---
+++ Review-Exercises/Data Stores and Persistence/EntitiesExercise/src/main/java/com/udacity/EntitiesExercise/EntitiesExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.EntitiesExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntitiesExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntitiesExerciseApplication.class, args);
+	}
+
+}
