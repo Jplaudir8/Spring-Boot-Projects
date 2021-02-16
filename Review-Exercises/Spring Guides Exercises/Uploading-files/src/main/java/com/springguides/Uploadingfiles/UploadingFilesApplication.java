0af3a814a72942144b0a0ca8a527ae335b343540
Joan-Perez-Lozano <joanperezl123@gmail.com>
2021-02-16 07:32:14 -0500

    swapped folders

--- Review-Exercises/Spring Guides Exercises/Uploading-files/src/main/java/com/springguides/Uploadingfiles/UploadingFilesApplication.java
+++ Review-Exercises/Spring Guides Exercises/Uploading-files/src/main/java/com/springguides/Uploadingfiles/UploadingFilesApplication.java
@@ -1,4 +1,4 @@
-package com.example.springguides.Uploadingfiles;
+package com.springguides.Uploadingfiles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
