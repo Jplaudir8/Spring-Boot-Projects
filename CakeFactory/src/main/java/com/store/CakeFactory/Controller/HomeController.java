1efa530d15b3e569c26669a1fa35283f9345ac1f
Joan-Perez-Lozano <joanperezl123@gmail.com>
2021-04-17 19:55:26 -0500

    added project

---
+++ CakeFactory/src/main/java/com/store/CakeFactory/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.store.CakeFactory.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String landingPage(Model model) {
+        return "index";
+    }
+}
---
+++ CakeFactory/src/test/java/com/store/CakeFactory/CakeFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.CakeFactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
