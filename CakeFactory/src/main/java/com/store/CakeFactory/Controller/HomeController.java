957addee923925b175f925b9f9cf5ff065142256
Joan-Perez-Lozano <joanperezl123@gmail.com>
2021-04-18 08:39:20 -0500

    modified directories

--- CakeFactory/src/main/java/com/store/CakeFactory/Controller/HomeController.java
+++ CakeFactory/src/main/java/com/store/CakeFactory/Controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String landingPage(Model model) {
+    public String landingPage() {
         return "index";
     }
 }
